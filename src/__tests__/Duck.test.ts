7b0966a6803d94a14f3b205e5634bffec92e8877
Yannick Fricke <yannickfricke@googlemail.com>
2020-05-05 21:51:16 +0200

    Added the duck test

---
+++ src/__tests__/Duck.test.ts
@@ -0,0 +1,105 @@
+import { Duck } from '../Duck';
+
+interface Test {
+    name: string;
+}
+
+describe('Duck', () => {
+    it('should be instantiable', () => {
+        const duck = new Duck({
+            namespace: '',
+            storePath: '',
+            actions: [],
+            actionCreators: () => ({}),
+            initialState: {},
+            reducer: () => ({}),
+        });
+
+        expect(duck).toBeDefined();
+    });
+
+    it('should return namespaced actions', () => {
+        const namespace = 'quacked';
+        const storePath = 'test';
+
+        const duck = new Duck({
+            namespace,
+            storePath,
+            actions: ['CREATE', 'READ', 'UPDATE', 'DELETE'],
+            actionCreators: () => ({}),
+            initialState: {},
+            reducer: () => ({}),
+        });
+
+        expect(duck.actions.CREATE).toEqual(`${namespace}/${storePath}/CREATE`);
+        expect(duck.actions.READ).toEqual(`${namespace}/${storePath}/READ`);
+        expect(duck.actions.UPDATE).toEqual(`${namespace}/${storePath}/UPDATE`);
+        expect(duck.actions.DELETE).toEqual(`${namespace}/${storePath}/DELETE`);
+    });
+
+    describe('reducer', () => {
+        it('should be a function', () => {
+            const duck = new Duck({
+                namespace: '',
+                storePath: '',
+                actions: [],
+                actionCreators: () => ({}),
+                initialState: {},
+                reducer: () => ({}),
+            });
+
+            expect(duck.getReducer).toBeDefined();
+            expect(typeof duck.getReducer).toBe('function');
+        });
+
+        it('should return the initial state', () => {
+            const initialState = {
+                quacked: 'test',
+            };
+
+            const duck = new Duck({
+                namespace: 'quacked',
+                storePath: 'test',
+                actions: [],
+                initialState,
+                reducer: (state, action, duck1) => state,
+                actionCreators: () => ({}),
+            });
+
+            const result = duck.getReducer(undefined, { type: 'testing' });
+
+            expect(result).toEqual(initialState);
+        });
+
+        it('should return the transformed state', () => {
+            const initialState = {
+                quacked: 'test',
+            };
+
+            const duck = new Duck({
+                namespace: 'quacked',
+                storePath: 'test',
+                actions: ['UPDATE'],
+                initialState,
+                reducer: (state, action, duck1) => {
+                    switch (action.type) {
+                        case duck1.actions.UPDATE:
+                            return {
+                                ...state,
+                                quacked: 'quacked',
+                            };
+                    }
+
+                    return state;
+                },
+                actionCreators: () => ({}),
+            });
+
+            const result = duck.getReducer(initialState, {
+                type: duck.actions.UPDATE,
+            });
+
+            expect(result).toEqual({ quacked: 'quacked' });
+        });
+    });
+});
